f12a69a19927ff785d0f539c8c52cd36d8cfb2cd
monaco <arianitkukaj@gmail.com>
2023-06-24 19:24:43 +0200

    Update paste.pl
    Responsive mode for any device now is avaliable !

--- paste.pl
+++ paste.pl
@@ -83,7 +83,10 @@ __DATA__
 <!DOCTYPE html>
 <html>
 <head>
-    <title>KISSmo Perl Version 1.1 stable</title>
+<meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta name="HandheldFriendly" content="true">
+    <title>KISSmo Perl Version 0.7 stable</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
     <style>
         body {
@@ -118,7 +121,7 @@ __DATA__
 </head>
 <body>
     <div class="container">
-        <h1>KISSmo Perl Version 1.1 stable</h1>
+        <h1>KISSmo Perl Version 0.7 stable</h1>
         <form method="post">
             <div class="form-group">
                 <textarea name="content" class="form-control" placeholder="Enter your content" rows="10"></textarea>
@@ -140,6 +143,9 @@ __DATA__
 <!DOCTYPE html>
 <html>
 <head>
+<meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta name="HandheldFriendly" content="true">
     <title>Paste</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
     <style>
