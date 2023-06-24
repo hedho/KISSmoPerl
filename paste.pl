992dc8a4588c02f13130955dec79af8495fe24a9
monaco <arianitkukaj@gmail.com>
2023-06-24 20:58:56 +0200

    Update paste.pl

--- paste.pl
+++ paste.pl
@@ -132,7 +132,7 @@ __DATA__
         </form>
     </div>
     <footer class="footer">
-        <p>This pastebin script has been developed by Arianit. The source code can be found at <a href="https://github.com/hedho">GitHub</a>.</p>
+        <p>This pastebin script has been developed by Arianit. The source code can be found at <a href="https://github.com/hedho/KISSmoPerl">GitHub</a>.</p>
     </footer>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
